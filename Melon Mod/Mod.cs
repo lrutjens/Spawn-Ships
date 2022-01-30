aeedf6c3c6b89ea8bad6069b1c5bc3864b97fca7
BuildTools <unconfigured@null.spigotmc.org>
2022-01-30 15:52:42 -0600

    Add project files.

---
+++ Melon Mod/Mod.cs
@@ -0,0 +1,42 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+namespace MyProject
+{
+    public class MyMod : MelonMod
+    {
+
+        public static MelonPreferences_Category spawnShip;
+        public static MelonPreferences_Entry<int> shipsToSpawn;
+
+        public override void OnApplicationStart()
+        {
+            spawnShip = MelonPreferences.CreateCategory("Spawn Ships");
+            shipsToSpawn = spawnShip.CreateEntry("Ships to Spawn", 1);
+        }
+
+        public override void OnUpdate()
+        {
+            int i = 0;
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                if (NetworkManager.instance.IsRoomPrivate() == true)
+                {
+                    int teamId = GameManager.instance.GetTeamID(NetworkManager.instance.primaryLocalPlayerID);
+                    while (i < 10)
+                    {
+                        TeamManager.instance.GetTeam(teamId).TryProduceShip();
+                        i++;
+                    }
+                    i = 0;
+                    LoggerInstance.Msg("You just pressed T");
+
+                }
+
+            }
+        }
+    }
+}
